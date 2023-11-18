633c624cb6489f6adcf4369f312f3f17b0a969bc
dixit5302 <135499321+dixit5302@users.noreply.github.com>
2023-11-18 18:22:58 +0530

    Update index.js

--- color generator/project/caGeEyUk/index.js
+++ color generator/project/caGeEyUk/index.js
@@ -1,6 +1,6 @@
 let characters = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
 let clickBtn = document.getElementById("click-btn")
-let bgdColor = document.querySelector("#bkg-color")
+let bgdColor = document.querySelector("#bgd-color")
 let body = document.querySelector("body")
 
 
