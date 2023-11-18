2d671d817e7e93f33938c21ecef739a5bc377c0d
dixit5302 <135499321+dixit5302@users.noreply.github.com>
2023-11-18 18:18:56 +0530

    Add files via upload

---
+++ color generator/project/caGeEyUk/index.js
@@ -0,0 +1,16 @@
+let characters = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+let clickBtn = document.getElementById("click-btn")
+let bgdColor = document.querySelector("#bkg-color")
+let body = document.querySelector("body")
+
+
+clickBtn.addEventListener("click", function(){
+        let getHex = '#'
+        for(let i = 0; i< 6; i++){
+        let randomNumbers = Math.floor(Math.random()*characters.length)
+        getHex += characters[randomNumbers]
+  }
+  body.style.backgroundColor = getHex
+  bgdColor.innerText = getHex
+})
+
